d5c2af9cc58493c7ece2757b782ec36f434e9627
Pranav <54665036+Pranav016@users.noreply.github.com>
2021-01-07 11:33:11 +0530

    Added functionality to save items to the List

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 
 function App() {
+  const [inputText, setInputText] = React.useState("");
+  const [items, setItems] = React.useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function storeValue() {
+    setItems((prevValue) => {
+      return [...prevValue, inputText];
+    });
+    setInputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={storeValue}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
